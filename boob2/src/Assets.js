809950ca79f758ba08abc2e644891b50cca807a7
Brad Henderson <leaveluck2heaven@gmail.com>
2012-12-26 16:07:14 -0500

    trigger loader

--- boob2/src/Assets.js
+++ boob2/src/Assets.js
@@ -1,10 +1,12 @@
 var  logo           = "assets/logo.png";
 var  boob1      = "assets/boob1.png";
 
+var sfx1               ="asset/sample.wav";
+
 var g_ressources = [
     //image
     {type:"image", src:logo             },
-    {type:"image", src:boob1         }
+    {type:"image", src:boob1         },
     //plist
 
     //fnt
@@ -12,6 +14,6 @@ var g_ressources = [
     //tmx
 
     //bgm
-
+    {type:"bgm", src:sfx1         }
     //effect
 ];
