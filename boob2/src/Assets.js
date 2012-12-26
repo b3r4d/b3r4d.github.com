7ec55362eecd80b912983873e989b93ce32393bd
Brad Henderson <leaveluck2heaven@gmail.com>
2012-12-26 15:56:14 -0500

    boobland init

---
+++ boob2/src/Assets.js
@@ -0,0 +1,17 @@
+var  logo           = "assets/logo.png";
+var  boob1      = "assets/boob1.png";
+
+var g_ressources = [
+    //image
+    {type:"image", src:logo             },
+    {type:"image", src:boob1         }
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
