543b817367b6d125fa26129af92ba906dec83593
Brad Henderson <leaveluck2heaven@gmail.com>
2013-01-16 16:49:22 -0500

    class example

---
+++ paradolia/src/cosmos/CosmosTest.js
@@ -0,0 +1,59 @@
+var Cosmos = cc.Class.extend({
+     source:null,
+     control:null,
+     content:null,
+
+ctor: function( ) 
+{
+this._super();
+Object.defineProperty( this, "creationList", { get:getCreationList(), set:setCreationList() });
+ },
+    
+//GETTERS AND SETTERS BEGIN
+getCreationList:function()
+{
+        
+},
+         
+setCreationList:function()
+{
+        
+},
+
+//PUBLIC FUNCTIONS
+load:function()
+{
+         
+},
+
+execute:function()
+{
+         
+} ,
+
+awake:function()
+{
+         
+},
+
+destroy:function()
+ {
+     
+ },
+ 
+appear:function()
+{
+          
+ } ,
+
+addAvatar:function ( ) 
+{
+
+},
+
+removeAvatar:function()
+{
+     
+}
+
+});
